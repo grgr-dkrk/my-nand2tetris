85b285afb5072eb8690b7d6720bfe57461df8dac
grgr-dkrk <40130327+grgr-dkrk@users.noreply.github.com>
2020-05-07 22:36:30 +0900

    simplefunction

--- projects/07/stack.ts
+++ projects/07/stack.ts
@@ -1,12 +1,26 @@
 // TODO
 export const StackManager = (() => {
   let index = 0
+  let callId = 0
+  let currentFunctionName = ''
   return {
     addIndex() {
       index++
     },
     getIndex() {
       return index
-    }
+    },
+    addCallId() {
+      callId++
+    },
+    getCallId() {
+      return callId
+    },
+    getCurrentFunctionName() {
+      return currentFunctionName
+    },
+    setCurrentFunctionName(arg: string) {
+      currentFunctionName = arg
+    },
   }
 })()
